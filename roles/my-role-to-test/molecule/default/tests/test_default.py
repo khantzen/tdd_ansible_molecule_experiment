3d3d8b5e31f2f6bf70c287f72e733b8c6f953ee5
Your Name <you@example.com>
2021-07-27 18:51:39 +0200

    first commit

---
+++ roles/my-role-to-test/molecule/default/tests/test_default.py
@@ -0,0 +1,15 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+def test_nginx_should_be_installed(host):
+    nginx = host.package("nginx")
+
+    assert nginx.is_installed
